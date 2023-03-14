063ebeaabcdaf9ac0bd2a0e7f8e48c8407d2d1d8
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-14 22:47:40 +0100

    Add solution 4 first task

--- src/Classes/Human.java
+++ src/Classes/Human.java
@@ -1,18 +1,28 @@
-import Classes.Human;
-
-public class Main {
-
-  /*Задача 1
+package Classes;
+/*Задача 1
 Создайте класс "Человек", конструктор которого принимает имя.
 При попытке вывести объект этого класса на экран должна выводиться строка "Человек по имени …".
 Добавьте в класс "Человек" метод "приветствие", который будет выводить строку: "Я родился!"
 Создайте классы "Мужчина" и "Женщина", которые будут наследниками класса "Человек".
 Попытка вывести их на экран должна приводить к выводу строки "Мужчина по имени …" или "Женщина по имени …"
 Метод "приветствие" класса "Женщина" должен выводить строку: "Я родилась!"*/
-  public static void main(String[] args) {
-    Human newHuman = new Human("Joseph");
-    System.out.println(newHuman);
+public class Human {
+  private String name;
+  public Human(String name)
+  {
+    this.name=name;
+  }
+  @Override
+  public String toString()
+  {
+    return String.format("Человек по имени %s", name);
+  }
 
+  public void setName(String name) {
+    this.name = name;
+  }
 
+  public String getName() {
+    return name;
   }
 }
